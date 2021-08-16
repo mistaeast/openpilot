9b8ee10c128f82cd6ee4b4967f82ecc98fd5e8cb
DFyffe <fyffed@netzero.net>
2021-08-16 06:39:51 -0400

    smartmdps toggle

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -44,6 +44,7 @@ def manager_init():
     ("UseClusterSpeed", "1"),
     ("LongControlEnabled", "0"),
     ("MadModeEnabled", "1"),
+    ("SmartMDPS", "0"),
     ("IsLdwsCar", "0"),
     ("LaneChangeEnabled", "1"),
     ("AutoLaneChangeEnabled", "1"),
